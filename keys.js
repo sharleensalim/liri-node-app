55f07cd40170cbeadc091829ade815a46ea4dacf
Sharleen Salim <sharleensalim@Sharleens-MacBook-Pro.local>
2017-10-25 16:56:29 -0700

    initial setup / commit

---
+++ keys.js
@@ -0,0 +1,10 @@
+console.log('this is loaded');
+
+var twitterKeys = {
+  consumer_key: '<IRRJtfM3uH867ojbcbtjRbDWr>',
+  consumer_secret: '<tSI4fxqVxz0sjDv81Be1t5tYuqtSUHwCDy6xiEb1e2ARTQ5vSc>',
+  access_token_key: '<923332742531522561-w9fAt0n6KeqBqgP1FzspyZOiygOtao8>',
+  access_token_secret: '<EoV8GjNtPaBTxA2KyhJtA3TjVjGZOxDjgZUszzlyf54sV>',
+}
+
+module.exports = twitterKeys;
